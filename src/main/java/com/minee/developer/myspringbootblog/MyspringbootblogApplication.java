996e4a221f30fad1ac7edad6ff654dc876f00111
[vmineev] <[minhee1806@gmail.com]>
2023-06-20 01:07:36 +0900

    환경세팅완료

---
+++ src/main/java/com/minee/developer/myspringbootblog/MyspringbootblogApplication.java
@@ -0,0 +1,13 @@
+package com.minee.developer.myspringbootblog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyspringbootblogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyspringbootblogApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/minee/developer/myspringbootblog/MyspringbootblogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.minee.developer.myspringbootblog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyspringbootblogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
